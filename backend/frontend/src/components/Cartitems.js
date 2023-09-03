d75791ee3d73d3f46f47318f645b4eaa74a96bac
Tapan Kumar <90493845+AlexCrew007@users.noreply.github.com>
2023-09-03 12:32:03 +0530

    Update Cartitems.js

--- backend/frontend/src/components/Cartitems.js
+++ backend/frontend/src/components/Cartitems.js
@@ -1,104 +1,101 @@
-import React from 'react'
-import {BsFillCartFill} from 'react-icons/bs'
-import { NavLink, useNavigate } from 'react-router-dom'
-import image from '../assets/lms-icon.png'
-import { useDispatch, useSelector } from 'react-redux'
-import { LoginSlice } from '../redux/slices/LoginSlice'; 
-import { toast } from 'react-hot-toast'
+import { useDeleteCartItemMutation } from '../redux/services/api';
 import { useFetchCartItemsQuery } from '../redux/services/api';
-import { Spinner } from './Spinner'
+import { useUpdateCartMutation } from '../redux/services/api';
+import Button from '@mui/material/Button';
+import React, { useState } from 'react'
+import {AiFillDelete} from 'react-icons/ai';
+import { toast } from 'react-hot-toast';
 
 
-export const Navbar = () => {
+export const CartItem = (item) => {
 
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-  
-  const user = useSelector( (state) => state.login.user)
   const access_token = JSON.parse(localStorage.getItem('access_token'))
-  const cartItems = useFetchCartItemsQuery(access_token, {
-    skip: !user, 
-  });
+  const cartItem = useFetchCartItemsQuery(access_token)
 
+  const booksItem=item.book
 
-  async function logoutHandler(e){
-    e.preventDefault()
-    dispatch(LoginSlice.actions.logout())
-    navigate('/login')
-    toast.error('logged out!')
+  
+  const [readMore,  setReadMore] = useState(true)
+  const [deleteCartItem] = useDeleteCartItemMutation()
+  const [updateCart, useUpdateCartMutationState ] = useUpdateCartMutation()
+
+  //Delete Handler delete items from cart
+  async function deleteHandler() {
+    try {
+      await deleteCartItem({ access_token: access_token, id: item.book.id })
+      toast.success('Item deleted!');
+      cartItem.refetch()
+    } catch (error) {
+      console.error('Error deleting item:', error);
+    }
   }
+
+
+  //Handle Qunatity or Partially upadte Cart Items
+  async function quantityHandler(e){
+    e.preventDefault()
+    const type = e.target.name
+    let id = item.book.id
+    if(type==='inc'){
+      let quantity = item.quantity+1
+      await updateCart({id,access_token,quantity})
+    }
+    else{
+    let quantity = item.quantity-1
+    if(quantity>=1){
+      await updateCart({id,access_token,quantity})
+      cartItem.refetch();
+    }
+    }
+    cartItem.refetch();
+  }
+
+  
+
   return (
-    <div>
-    <nav className='flex  items-center justify-between h-20 max-w-6xl mx-auto'>
-      <NavLink to={'/'}>
-      <div className='ml-5'>
-      <img  className='h-14' src={image} alt='nav'></img>
-      </div>
-      </NavLink>
-      {
-        user &&
-        <p className='text-slate-200'>Hiii {user}</p>
-      }
-      <div className='flex items-center  font-medium text-slate-100 ml-20 '>
-      
-        <NavLink to={'/'}>
-        <p> Home </p> 
-        </NavLink>
-      </div>
-      <div className='flex items-center  font-medium text-slate-100 ml-20 '>
-        {
-          user ? <button onClick={logoutHandler}>Logout</button>
-          :
-          <NavLink to={'/login'}>
-          <p> Login </p> 
-          </NavLink>
-        }
-      </div>
-      {
-        !user &&
-        <div className='flex items-center  font-medium text-slate-100 ml-20 '>
-        <NavLink to={'/signup'}>
-        <p> Signup </p> 
-        </NavLink>
-      </div>
-      }
-      
-      {
-        user && 
-        <div className='flex items-center  font-medium text-slate-100 ml-20 '>
-        <NavLink to={'/profile'}>
-        <p> Profile </p> 
-        </NavLink>
-      </div>
-      }
-      {
-        user && 
-        <div className='flex items-center  font-medium text-slate-100 ml-20 '>
-        <NavLink to={'/orders'}>
-        <p> Your Orders </p> 
-        </NavLink>
-      </div>
-      }
-      
-      {
-        user &&
-          <NavLink to={'/cart'}>
-            <div className='text-white relative'>
-                <BsFillCartFill className='text-2xl'></BsFillCartFill>
-                {cartItems.isLoading ? (
-                    <span className='absolute -top-1 -right-4' ><Spinner></Spinner></span>
-                    ) : 
-                    cartItems.isSuccess && cartItems.data && cartItems.data.length > 0 &&
-                     (
-                    <span className='absolute -top-1 -right-2 bg-green-600 text-xs w-5 h-5 flex justify-center items-center animate-bounce rounded-full text-white'>
-                      {cartItems.data.length}
-                    </span>
-                    )}
+    <div className='flex mt-6  items-center '>
+    { booksItem && 
+      <div className='flex justify-between items-center '>
+        <div className='flex w-full h-[250px] m-5 '>
+          <img className='w-full'  src={booksItem.image} alt='img'/>
+        </div>
+        <div className=' ml-5 w-full'>
+        <div className='mb-3'>
+          <p className='text-gray-700  font-semibold text-2xl text-left 
+            truncate  mt-1'>{booksItem.title}</p>
+        </div>
+        <div>
+        <p className=' font-normal w-80 text-left text-[15px] text-slate-600' >
+        { readMore ? booksItem.description.slice(0,200) : booksItem.description}
+          { booksItem.description.length> 200 &&
+            <span className='text-blue-600 cursor-pointer' onClick={()=>setReadMore(!readMore)}> 
+            {readMore ? '...read more' : '...show less'}
+          </span>
+          }
+        </p>
+        <p className='font-semibold'><span className='text-green-600 font-semibold'>{booksItem.category}</span> </p>
+
+        
+        </div>
+          <div>
+          <div className='flex justify-between gap-12 items-center w-full mt-10'>
+          <div className='flex justify-between '>
+            <p className='text-green-600 font-semibold'>{booksItem.price} <span>₹</span></p>
+          </div>
+          </div>
+          <p> Quantity :</p>
+          <div className='flex'>
+          <Button name='inc' onClick={quantityHandler}>+</Button>
+            <p className='font-semibold'> <span className='text-green-600 font'>{item.quantity}</span></p>
+            <Button name='dec' onClick={quantityHandler}>-</Button>
+          </div>
+          <div className='relative text-[40px] '>
+            <button className='absolute -top-8 -right-2 rounded-full bg-red-400 text-3xl' onClick={deleteHandler}><AiFillDelete/></button>
             </div>
-          </NavLink>
-      }
-      
-    </nav>
+          </div>
+        </div>
+      </div> 
+    }
     </div>
   )
 }
